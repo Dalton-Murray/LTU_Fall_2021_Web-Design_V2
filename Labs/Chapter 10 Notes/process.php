3725684406e3bfa8be371fb63d24fc876636a248
Dalton-Murray <murraydalton1@comcast.net>
2021-10-22 12:59:55 -0400

    Notes

--- Labs/Chapter 10 Notes/process.php
+++ Labs/Chapter 10 Notes/process.php
@@ -1,6 +1,21 @@
-<?php
+<!--- Dalton Murray -->
 
-echo "Hello World";
+<!doctype html>
+<html lang="en">
+<head>
 
+	<meta charset="utf-8">
+	<title>Title at top of tab</title>
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
 
->
+</head>
+<body>
+
+	<?php
+		$username = $_REQUEST['username'];
+		$password = $_REQUEST['password'];
+		echo "Your username is: " .$username. " and your password is: " .$password;
+	>
+
+</body>
+</html>
